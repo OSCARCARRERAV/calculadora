6278cb1ea7ac0eee79bffbc8061e98ca547d69a9
Francisco Manso <oscar.carrera@outlook.com>
2022-02-28 08:48:51 +0100

    Added Controller
    Added controller
    Added Request DTO

---
+++ src/main/java/com/test/calculadora/DTO/RequestDTO.java
@@ -0,0 +1,32 @@
+package com.test.calculadora.DTO;
+
+
+public class RequestDTO {
+
+	
+	String operand1;
+	String operand2;
+	Character operator;
+	
+	public String getOperand1() {
+		return operand1;
+	}
+	public void setOperand1(String operand1) {
+		this.operand1 = operand1;
+	}
+	public String getOperand2() {
+		return operand2;
+	}
+	public void setOperand2(String operand2) {
+		this.operand2 = operand2;
+	}
+	public Character getOperator() {
+		return operator;
+	}
+	public void setOperator(Character operator) {
+		this.operator = operator;
+	}
+	
+	
+		
+}
---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,48 @@
+package controller;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.test.calculadora.DTO.RequestDTO;
+import com.test.calculadora.exception.NotAcceptableException;
+import com.test.calculadora.service.CalculatorService;
+import com.test.calculadora.utils.Constants;
+
+
+
+@RestController
+@CrossOrigin
+@RequestMapping("/operations")
+public class Controller {
+
+	@Autowired
+	CalculatorService calcService;
+
+	@PostMapping("/operation")
+	public @ResponseBody BigDecimal executeOperation(@RequestBody RequestDTO request) throws NotAcceptableException {
+
+		String number1 = request.getOperand1();
+		String number2 = request.getOperand2();
+		Character operator = request.getOperator();
+
+		if (number1 != null && number2 != null && operator != null) {
+			return calcService.executeOperation(number1, number2, operator);
+		}
+
+		if (number1 == null || number2 == null) {
+			throw new NotAcceptableException(Constants.MISSING_NUMBER);
+		}
+
+		else {
+			throw new NotAcceptableException(Constants.MISSING_OPERATOR);
+		}
+
+	}
+}
